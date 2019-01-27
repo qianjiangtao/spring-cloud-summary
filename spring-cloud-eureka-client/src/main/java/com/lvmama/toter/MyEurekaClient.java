e62d5fd45a7b5892d8f3744379840ec9b93cbf04
qianjiangtao <qianjiangtao@lvmama.com>
2019-01-27 21:06:19 +0800

    eureka commit

--- spring-cloud-eureka-client/src/main/java/com/lvmama/toter/MyEurekaClient.java
+++ spring-cloud-eureka-client/src/main/java/com/lvmama/toter/MyEurekaClient.java
@@ -2,6 +2,7 @@ package com.lvmama.toter;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /**
@@ -9,7 +10,7 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
  * @since 2019-01-26 14:33
  **/
 @SpringBootApplication
-@EnableEurekaClient
+@EnableDiscoveryClient
 public class MyEurekaClient {
     public static void main(String[] args) {
         SpringApplication.run(MyEurekaClient.class, args);
